ef8502236bd4c4c66e70dd12b7057ffb6669adc3
Alex Parog <sbibik2000@mail.ru>
2025-05-11 18:56:17 +0300

    feat: refactor MagaCourseServiceApplication

--- maga-course-service/src/main/java/ru/parog/magacourseservice/MagaCourseServiceApplication.java
+++ maga-course-service/src/main/java/ru/parog/magacourseservice/MagaCourseServiceApplication.java
@@ -2,8 +2,12 @@ package ru.parog.magacourseservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"ru.parog.magacourseservice.entity", "ru.parog.onlinelearningplatformmodel.entity"})
+@EnableJpaRepositories(basePackages = {"ru.parog.magacourseservice.repository"})
 public class MagaCourseServiceApplication {
 
     public static void main(String[] args) {
