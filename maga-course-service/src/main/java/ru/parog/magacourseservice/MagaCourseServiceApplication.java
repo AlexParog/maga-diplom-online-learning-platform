806cc03597bce0755bf33833772f9c7b42e0cfa7
Alex Parog <sbibik2000@mail.ru>
2025-05-11 14:40:55 +0300

    init course-service

---
+++ maga-course-service/src/main/java/ru/parog/magacourseservice/MagaCourseServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.parog.magacourseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagaCourseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagaCourseServiceApplication.class, args);
+    }
+
+}
---
+++ maga-course-service/src/test/java/ru/parog/magacourseservice/MagaCourseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.parog.magacourseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagaCourseServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
