656b17c1d0b42f3b24aef5fbe824fac5579e8365
Alex Parog <sbibik2000@mail.ru>
2025-05-11 14:43:24 +0300

    init submission-service

---
+++ maga-submission-service/src/test/java/ru/parog/magasubmissionservice/MagaSubmissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.parog.magasubmissionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagaSubmissionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
