2d8405e6483fedf6249a717b04209fe1d4c1cbbc
Alex Parog <sbibik2000@mail.ru>
2025-05-11 14:44:40 +0300

    init online-learning-platform-model

---
+++ online-learning-platform-model/src/main/java/ru/parog/onlinelearningplatformmodel/OnlineLearningPlatformModelApplication.java
@@ -0,0 +1,13 @@
+package ru.parog.onlinelearningplatformmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLearningPlatformModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineLearningPlatformModelApplication.class, args);
+	}
+
+}
