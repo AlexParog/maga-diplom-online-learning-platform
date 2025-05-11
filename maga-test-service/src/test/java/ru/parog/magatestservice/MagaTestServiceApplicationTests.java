78cd26d3ab2a30e7fa536ff676babccd4b5cc6a1
Alex Parog <sbibik2000@mail.ru>
2025-05-11 14:42:17 +0300

    init test-service

---
+++ maga-test-service/src/test/java/ru/parog/magatestservice/MagaTestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.parog.magatestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagaTestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
